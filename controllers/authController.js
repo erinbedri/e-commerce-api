402fa0d1d351bbc13a9b32148aebf269d6ce2175
erinbedri <erinbedri@gmail.com>
2023-10-16 22:29:50 +0200

    reqister

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,18 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
 const register = async (req, res) => {
-    res.send("Register User");
+    const { name, email, password } = req.body;
+
+    const emailAlreadyExists = await User.findOne({ email });
+
+    if (emailAlreadyExists) {
+        throw new CustomError.BadRequestError("Email already exists");
+    }
+
+    const user = await User.create({ name, email, password });
+    res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
