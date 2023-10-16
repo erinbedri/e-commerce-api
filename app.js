59f03182bd36fa18bb76865db4ad96f1518f5dd7
erinbedri <erinbedri@gmail.com>
2023-10-16 16:38:41 +0200

    install other packages

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
+require("dotenv").config();
+require("express-async-errors");
+
+// express
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3000;
+// rest of the packages
+const morgan = require("morgan");
+
+// db
+const connectDB = require("./db/connect");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("e-commerce-api");
+});
+
+app.use(notFoundMiddleware);
+app.unsubscribe(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URL);
         app.listen(port, console.log(`Server is listening on port ${port}...`));
     } catch (error) {
         console.log(error);
